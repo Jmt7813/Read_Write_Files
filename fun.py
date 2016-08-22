e90876eae39b4fc58630d2a8ba987c07157921b0
Jmt7813 <jmt1183@gmail.com>
2016-08-22 15:48:36 -0700

    Update fun.py

--- fun.py
+++ fun.py
@@ -1,5 +1,5 @@
 from sys import argv
-import time
+
 
 script, filename = argv
 
@@ -10,30 +10,20 @@ print "To accept press RETURN"
 raw_input("?")
 
 print "Opening file ..."
-
 # without the 'w', I got "IOError: File not open for writing" when I tried to truncate.
 target = open(filename)
 #target = open(filename, 'w')
 
-time.sleep(1)
-
 print "Erasing contents..." 
-
 target.truncate()
 
-time.sleep(1)
-
 target.write(raw_input("Type out something to replace the erased contents.\n > "))
-
 print "Writing contents to the file"
-time.sleep(1)
-print "Closing the file..."
 
+print "Closing the file..."
 target.close()
-time.sleep(1)
+
 print "Ok, lets make sure those changes were saved properly"
 
 target = open(filename)
-
-time.sleep(1)
 print target.read()
